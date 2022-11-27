9d688f927ef41d42e4ba1451c03a5588905f5e99
altair961 <altair89@list.ru>
2022-11-27 14:25:32 +0300

    Restaurant sample app was added.

---
+++ Restaurant/Restaurant.RestApi.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Restaurant.RestApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
